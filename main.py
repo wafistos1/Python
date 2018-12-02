a1f5b22aa624cfcb840dabb18911210367e38d4a
wafistos <mameri.wafi@gmail.com>
2018-12-02 22:06:51 +0100

    add setter and getter

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ class Personne:
             avec une valeur par defaut ...
         """
         
-        self.nom = nom
-        self.prenom = prenom
+        self._nom = nom
+        self._prenom = prenom
         self.age = age
-        self.lieu_residence = lieu_residence
+        self._lieu_residence = lieu_residence
         Personne.personne_creer +=1#C'est un attribu statique 
     
     def se_presente(self):
@@ -35,9 +35,49 @@ class Personne:
             print("C'est pas le moment de dire salut!! il faut dire Bonjour !!")
         else:
             print("J'ai pas compris votre question il faut commencer pas dire Bonjour")
-
     
-   
+    def combien(cls):
+        """Methode de classe (Methode statique) qui compte le nombre de Personne creer
+        """
+        print("Jusqu'a present il y a {} Personnes creer.".format(cls.personne_creer))
+    
+    combien = classmethod(combien)
+
+
+    def _get_lieu_residence(self):
+        """Methode qui sera appelee quand on souhaitera acceder en lecture a l'attribut 'lieu_residence
+        """
+        return self._lieu_residence
+
+    def _set_lieu_residence(self, lieu):
+        """Methode qui sera appelee quand on souhaitera acceder en ecriture a l'attribut 'lieu_residence
+        """
+        print("Attention il semble que {} demenage de {} a {}.".format(self.nom, self.lieu_residence,lieu))
+        self._lieu_residence = lieu 
+
+
+    def _set_nom(self, nom):
+        """Setter pour l'attribut nom
+        """
+        print("Vous avez changer de nom de {} a {}.".format(self._nom, nom))
+        self._nom = nom
+    def _get_nom(self):
+        """Getter de l'attribut nom
+        """
+        return self._nom 
+
+    def _get_prenom(self):
+        """Getter de prenom
+        """
+        return self._prenom
+    def _set_prenom(self, prenom):
+        """Setter
+        """
+        print("Attention tu vas changer ton prenom de {} a {}.".format(self._prenom, prenom))
+
+    lieu_residence = property(_get_lieu_residence, _set_lieu_residence)# Il faut pas oublier """
+    nom = property(_get_nom, _set_nom)
+    prenom = property(_get_prenom, _set_prenom)
 
 """ Programme principal"""
 
@@ -53,5 +93,12 @@ Dupont.se_presente()
 
 print(Personne.personne_creer)
 
-wafi.se_parler("bonjour")
+wafi.se_parler("Salut")
+
+Personne.combien()#Methode de classe qui compte le nombre de Personne creer
+Dupont.lieu_residence = "caca"#setter
+print(Dupont.lieu_residence)#getter
+wafi.prenom = "wafistos"
+Dupont.nom = "dupont"
+
 
